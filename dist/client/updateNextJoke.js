6dec9fdb3382dd45c2c09a36e6d37225ca02f77a
jose <j6alopez@gmail.com>
2024-01-09 16:00:41 +0100

    feat: implement random joke - use of Math.random which returns 0 to 0.99

--- dist/client/updateNextJoke.js
+++ dist/client/updateNextJoke.js
@@ -7,12 +7,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+import { ChuckNorrisService } from '../domain/services/jokes/ChuckNorrisService.js';
 import { HazDadService } from '../domain/services/jokes/HazDadService.js';
 import { DefaultJokeScore } from '../domain/services/scores/DefaultJokeScore.js';
 import { jokesWithScores } from '../index.js';
 export function updateJoke() {
     return __awaiter(this, void 0, void 0, function* () {
-        const jokeService = new HazDadService();
+        const jokeService = randomizeService();
         const recievedJoke = yield jokeService.getJoke();
         const jokeElement = document.getElementById('joke');
         if (jokeElement !== null) {
@@ -20,6 +21,10 @@ export function updateJoke() {
             jokeElement.innerText = recievedJoke.value;
             jokeScoreService.trackJoke(recievedJoke);
             console.log(jokesWithScores);
+        }
+        function randomizeService() {
+            const random = Math.random() >= 0.5;
+            return random ? new HazDadService() : new ChuckNorrisService();
         }
     });
 }
--- src/client/updateNextJoke.ts
+++ src/client/updateNextJoke.ts
@@ -1,4 +1,5 @@
 import {Joke} from '../domain/entitites/Joke.js';
+import {ChuckNorrisService} from '../domain/services/jokes/ChuckNorrisService.js';
 import {HazDadService} from '../domain/services/jokes/HazDadService.js';
 import {JokeService} from '../domain/services/jokes/JokeService.js';
 import {DefaultJokeScore} from '../domain/services/scores/DefaultJokeScore.js';
@@ -6,7 +7,7 @@ import {JokeScoreService} from '../domain/services/scores/JokeScoreService.js';
 import {jokesWithScores} from '../index.js';
 
 export async function updateJoke(): Promise<void> {
-  const jokeService: JokeService = new HazDadService();
+  const jokeService: JokeService = randomizeService();
   const recievedJoke: Joke = await jokeService.getJoke();
 
   const jokeElement: HTMLElement | null = document.getElementById('joke');
@@ -15,5 +16,10 @@ export async function updateJoke(): Promise<void> {
     jokeElement.innerText = recievedJoke.value;
     jokeScoreService.trackJoke(recievedJoke);
     console.log(jokesWithScores);
+  }
+
+  function randomizeService(): JokeService {
+    const random: Boolean = Math.random() >= 0.5;
+    return random ? new HazDadService() : new ChuckNorrisService();
   }
 }
