97fb435684f4c254601c41d331fb946e913179d0
jose <j6alopez@gmail.com>
2024-01-11 17:06:10 +0100

    feat: first layout version stable

--- dist/index.js
+++ dist/index.js
@@ -1,26 +1,22 @@
-import { updateJoke } from './client/updateNextJoke.js';
-import { initializeContent } from './client/initializeContent.js';
-import { saveJokeScore } from './client/saveJokeScore.js';
-import { ScoreType } from './domain/enums/ScoreType.js';
-document.addEventListener('DOMContentLoaded', () => {
-    initializeContent();
-    const jokeButton = document.getElementById('next-joke');
-    jokeButton === null || jokeButton === void 0 ? void 0 : jokeButton.addEventListener('click', () => updateJoke());
-    const badScoreButton = document.getElementById('bad-score');
-    const normalScoreButton = document.getElementById('normal-score');
-    const goodScoreButton = document.getElementById('good-score');
-    badScoreButton === null || badScoreButton === void 0 ? void 0 : badScoreButton.addEventListener('click', () => {
-        saveJokeScore(ScoreType.BAD);
-        updateJoke();
-    });
-    normalScoreButton === null || normalScoreButton === void 0 ? void 0 : normalScoreButton.addEventListener('click', () => {
-        saveJokeScore(ScoreType.NORMAL);
-        updateJoke();
-    });
-    goodScoreButton === null || goodScoreButton === void 0 ? void 0 : goodScoreButton.addEventListener('click', () => {
-        saveJokeScore(ScoreType.GOOD);
-        updateJoke();
-    });
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   initializeContent();
+//   const jokeButton = document.getElementById('next-joke');
+//   jokeButton?.addEventListener('click', () => updateJoke());
+//   const badScoreButton = document.getElementById('bad-score');
+//   const normalScoreButton = document.getElementById('normal-score');
+//   const goodScoreButton = document.getElementById('good-score');
+//   badScoreButton?.addEventListener('click', () => {
+//     saveJokeScore(ScoreType.BAD);
+//     updateJoke();
+//   });
+//   normalScoreButton?.addEventListener('click', () => {
+//     saveJokeScore(ScoreType.NORMAL);
+//     updateJoke();
+//   });
+//   goodScoreButton?.addEventListener('click', () => {
+//     saveJokeScore(ScoreType.GOOD);
+//     updateJoke();
+//   });
+// });
 export const jokesWithScores = [];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,30 @@ import {saveJokeScore} from './client/saveJokeScore.js';
 import {JokeScore} from './domain/entitites/JokeScore.js';
 import {ScoreType} from './domain/enums/ScoreType.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  initializeContent();
+// document.addEventListener('DOMContentLoaded', () => {
+//   initializeContent();
 
-  const jokeButton = document.getElementById('next-joke');
-  jokeButton?.addEventListener('click', () => updateJoke());
+//   const jokeButton = document.getElementById('next-joke');
+//   jokeButton?.addEventListener('click', () => updateJoke());
 
-  const badScoreButton = document.getElementById('bad-score');
-  const normalScoreButton = document.getElementById('normal-score');
-  const goodScoreButton = document.getElementById('good-score');
+//   const badScoreButton = document.getElementById('bad-score');
+//   const normalScoreButton = document.getElementById('normal-score');
+//   const goodScoreButton = document.getElementById('good-score');
 
-  badScoreButton?.addEventListener('click', () => {
-    saveJokeScore(ScoreType.BAD);
-    updateJoke();
-  });
+//   badScoreButton?.addEventListener('click', () => {
+//     saveJokeScore(ScoreType.BAD);
+//     updateJoke();
+//   });
 
-  normalScoreButton?.addEventListener('click', () => {
-    saveJokeScore(ScoreType.NORMAL);
-    updateJoke();
-  });
+//   normalScoreButton?.addEventListener('click', () => {
+//     saveJokeScore(ScoreType.NORMAL);
+//     updateJoke();
+//   });
 
-  goodScoreButton?.addEventListener('click', () => {
-    saveJokeScore(ScoreType.GOOD);
-    updateJoke();
-  });
-});
+//   goodScoreButton?.addEventListener('click', () => {
+//     saveJokeScore(ScoreType.GOOD);
+//     updateJoke();
+//   });
+// });
 
 export const jokesWithScores: JokeScore[] = [];
